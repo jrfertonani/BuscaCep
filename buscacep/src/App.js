719aae16d9108697e0ad5a8c457beb6fa61ec094
jrfertonani <ademirjuniorfertonanigmail.com>
2022-04-09 01:40:09 -0300

    buscaCep.

--- buscacep/src/App.js
+++ buscacep/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react' ;
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+
 
 function App() {
+
+  const [input, setInput] = useState('')
+
+  function handleSearch(){
+    alert('valor do input' + input)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+       <h1 className="title">Busca Cep</h1>
+
+       <div className="containerInput">
+          <input type="text" placeholder="Digite seu cep..." 
+          value={ input }
+          onChange={(e) =>setInput(e.target.value)}
+          />
+          
+
+           <buttom className="buttonSearch" onClick={handleSearch}>
+            <FiSearch size={25} color='#a1a'/> 
+            </buttom>
+        </div>
+
+        <main className="main">
+          <h2>CEP: 87023702</h2>
+
+          <span>Rua pioneiro Jose</span>
+          <span>Complemento: A melhor e mais feliz Casa</span>
+          <span>js. Vtiria</span>
+          <span>Maringa</span>
+
+        </main>
+
+     </div>
   );
 }
 
